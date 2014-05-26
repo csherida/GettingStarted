c90a77043bfc0781cdc42c1f9983d90da7f315a4
csherida <chris@christophersheridan.com>
2014-05-26 12:17:36 -0400

    Step 5 - reusable grid

--- session_3_basic_grid/BasicRallyGrid/App.js
+++ session_3_basic_grid/BasicRallyGrid/App.js
@@ -6,8 +6,11 @@ Ext.define('CustomApp', {
     extend: 'Rally.app.App',      // The parent class manages the app 'lifecycle' and calls launch() when ready
     componentCls: 'app',          // CSS styles found in app.css
 
+	myStore: undefined,
+	myGrid: undefined,
+	
     // Entry Point to App
-    launch: function() {
+    launch: function() { 
 
 		console.log('our first app');     // see console api: https://developers.google.com/chrome-developer-tools/docs/console-api
       
@@ -71,45 +74,57 @@ Ext.define('CustomApp', {
 		
 		console.log('selected severity', selectedSeverityValue);
 		console.log('selected iter', selectedIterRef);
+		
+		var myFilters = [
+				{
+					property: 'Iteration',
+					operation: '=',
+					value: selectedIterRef
+				},
+				{
+					property: 'Severity',
+					operation: '=',
+					value: selectedSeverityValue
+				}
+			];
 
-		var myStore = Ext.create('Rally.data.wsapi.Store', {
-          model: 'Defect',
-          autoLoad: true,                         // <----- Don't forget to set this to true! heh
-		  filters:[
-			{
-				property: 'Iteration',
-				operation: '=',
-				value: selectedIterRef
-			},
-			{
-				property: 'Severity',
-				operation: '=',
-				value: selectedSeverityValue
-			}
-		  ],
-          listeners: {
-              load: function(myStore, myData, success) {
-                  console.log('got data!', myStore, myData);
-                  this._loadGrid(myStore);      // if we did NOT pass scope:this below, this line would be incorrectly trying to call _createGrid() on the store which does not exist.
-              },
-              scope: this                         // This tells the wsapi data store to forward pass along the app-level context into ALL listener functions
-          },
-          fetch: ['FormattedID', 'Name', 'Severity', 'Iteration']   // Look in the WSAPI docs online to see all fields available!
-        });
-
+		// if store exists, just load new data
+		if (this.defectStore) {
+			console.log('store exists');
+			this.defectStore.setFilter(myFilters);
+			this.defectStore.load();
+		} else {
+			// create store
+			console.log('creating store');
+			this.defectStore = Ext.create('Rally.data.wsapi.Store', {
+				model: 'Defect',
+				autoLoad: true,                         // <----- Don't forget to set this to true! heh
+				filters: myFilters,
+				listeners: {
+					  load: function(myStore, myData, success) {
+						  console.log('got data!', myStore, myData);
+						  if (!this.myGrid){
+							this._createGrid(myStore);      // if we did NOT pass scope:this below, this line would be incorrectly trying to call _createGrid() on the store which does not exist.
+						  }
+					  },
+					  scope: this                         // This tells the wsapi data store to forward pass along the app-level context into ALL listener functions
+				},
+				fetch: ['FormattedID', 'Name', 'Severity', 'Iteration']   // Look in the WSAPI docs online to see all fields available!
+			});
+		}
     },
 
     // Create and Show a Grid of given stories
-    _loadGrid: function(myStoryStore) {
+    _createGrid: function(myStoryStore) {
 
-      var myGrid = Ext.create('Rally.ui.grid.Grid', {
+      this.myGrid = Ext.create('Rally.ui.grid.Grid', {
         store: myStoryStore,
         columnCfgs: [         // Columns to display; must be the same names specified in the fetch: above in the wsapi data store
           'FormattedID', 'Name', 'Severity', 'Iteration'
         ]
       });
 
-      this.add(myGrid);       // add the grid Component to the app-level Container (by doing this.add, it uses the app container)
+      this.add(this.myGrid);       // add the grid Component to the app-level Container (by doing this.add, it uses the app container)
 
       console.log('what is this?', this);
 
